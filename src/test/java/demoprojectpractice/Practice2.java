d86d2ca337bdce744485facc75cb939914d826ef
Priyanka Ghosh <Priyanka Ghosh@Priyanka>
2023-05-27 00:29:07 +0530

    commit testscript2

--- src/test/java/demoprojectpractice/Practice2.java
+++ src/test/java/demoprojectpractice/Practice2.java
@@ -1,5 +1,12 @@
 package demoprojectpractice;
 
+import org.testng.annotations.Test;
+
 public class Practice2 {
+	@Test
+	public void name()
+	{
+		System.out.println("my name is priyanka");
+	}
 
 }
