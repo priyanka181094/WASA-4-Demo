20a1d3b2d5369caf7a862e3e51c521f72a5b6af7
Priyanka Ghosh <Priyanka Ghosh@Priyanka>
2023-05-26 18:46:00 +0530

    SampleTest  Testscript1 committed

---
+++ src/test/java/demoprojectpractice/Practice.java
@@ -0,0 +1,23 @@
+package demoprojectpractice;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	
+	@Test
+	public void insert()
+	{
+		System.out.println("insert  product");
+	}
+	@Test
+	public void delete()
+	{
+		System.out.println("delete product");
+	}
+	@Test
+	public void create()
+	{
+		System.out.println("create product");
+	}
+
+}
